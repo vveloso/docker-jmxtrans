fdd6088ac55040428897cd1e367f628a301910c9
Vasco Veloso <vveloso@gmail.com>
2016-05-16 14:33:36 +0100

    Added custom Elasticsearch JMX output writer.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --build-arg HTTP_PROXY=http://proxy-emea.intra.coriant.com:81  -t vveloso/jmxtrans:latest ./image
+docker build --build-arg HTTP_PROXY=http://proxy-emea.intra.coriant.com:81 --build-arg HTTPS_PROXY=https://proxy-emea.intra.coriant.com:81 -t vveloso/jmxtrans:latest ./image
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name jmxtrans -v /c/Users/vveloso/jmxtrans.docker/log:/opt/jmxtrans/log -v /c/Users/vveloso/jmxtrans.docker/conf:/opt/jmxtrans/conf vveloso/jmxtrans $1
+docker run --name jmxtrans -v /c/Users/vveloso/jmxtrans.docker/log:/opt/jmxtrans/log -v /c/Users/vveloso/jmxtrans.docker/conf:/opt/jmxtrans/conf -d vveloso/jmxtrans $1
